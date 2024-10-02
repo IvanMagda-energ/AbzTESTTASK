97b2935b4f4b2dbc79c6e92cb284bb1b9e151257
IvanMagda <ivanmagda164@gmail.com>
2024-10-02 23:01:29 +0300

    Initial Commit

---
+++ AbzTESTTASK/AbzTESTTASKApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbzTESTTASKApp.swift
+//  AbzTESTTASK
+//
+//  Created by Ivan Magda on 01.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct AbzTESTTASKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
