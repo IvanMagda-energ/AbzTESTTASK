269f31ff5ec2d984623ec05204ccd9a0c0bd584c
IvanMagda <ivanmagda164@gmail.com>
2024-10-05 00:13:18 +0300

    implement no connection view

--- AbzTESTTASK/ContentView.swift
+++ AbzTESTTASK/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var connectionMonitor = MonitorInternetConnection()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Color.clear
+            .fullScreenCover(isPresented: $connectionMonitor.isOffline) {
+                NoConnectionView()
+            }
     }
 }
 
